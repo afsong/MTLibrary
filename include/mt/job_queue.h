1fb45fb3db4e5f498f7639ea217e88ac9c97b50e
Annie Song <afsong2018@gmail.com>
2023-03-10 23:54:16 -0500

    reformat files

--- include/mt/job_queue.h
+++ include/mt/job_queue.h
@@ -2,14 +2,12 @@
 #define __job__queue_H__
 
 #include <cstddef>
-#include <queue>
 #include <mutex>
+#include <queue>
 
 namespace mt {
-template <typename T>
-class job_queue
-{
-public:
+  template <typename T> class job_queue {
+  public:
     job_queue(size_t cap);
     ~job_queue();
 
@@ -19,60 +17,40 @@ public:
     size_t size() const;
     size_t cap() const;
 
-private:
+  private:
     std::queue<T> task_queue;
     std::mutex mutex;
     size_t capacity;
+  };
 
-};
+  template <typename T> job_queue<T>::job_queue(size_t cap)
+      : task_queue(), mutex(), capacity(cap) {}
 
-template <typename T>
-job_queue<T>::job_queue(size_t cap)
-    : task_queue(), mutex(), capacity(cap)
-{
-}
+  template <typename T> job_queue<T>::~job_queue() {}
 
-template <typename T>
-job_queue<T>::~job_queue()
-{
-}
-
-template <typename T>
-void job_queue<T>::add_job(const T &job)
-{
+  template <typename T> void job_queue<T>::add_job(const T &job) {
     std::lock_guard<std::mutex> lock(mutex);
 
-    if (task_queue.size() < capacity)
-    {
-        task_queue.push(job);
+    if (task_queue.size() < capacity) {
+      task_queue.push(job);
     }
-}
+  }
 
-template <typename T>
-T job_queue<T>::pop_job()
-{   
+  template <typename T> T job_queue<T>::pop_job() {
     std::lock_guard<std::mutex> lock(mutex);
-    if(task_queue.size()<=0){
-        return T();
+    if (task_queue.size() <= 0) {
+      return T();
     }
     T ret = task_queue.front();
     task_queue.pop();
-    
+
     return ret;
-}
+  }
 
-template <typename T>
-size_t job_queue<T>::size() const
-{
-    return task_queue.size();
-}
+  template <typename T> size_t job_queue<T>::size() const { return task_queue.size(); }
 
+  template <typename T> size_t job_queue<T>::cap() const { return capacity; }
 
-template <typename T>
-size_t job_queue<T>::cap() const{
-    return capacity;
-}
-
-}
+}  // namespace mt
 
 #endif
--- test/source/job_queue.t.cpp
+++ test/source/job_queue.t.cpp
@@ -1,83 +1,76 @@
-#include <mt/job_queue.h>
 #include <doctest/doctest.h>
+#include <mt/job_queue.h>
+
 #include <thread>
 
-TEST_CASE("TestJobQueueConstructor")
-{
-    mt::job_queue<int> jq(100);
+TEST_CASE("TestJobQueueConstructor") {
+  mt::job_queue<int> jq(100);
 
-    CHECK_EQ(jq.size(), 0);
-    CHECK_EQ(jq.cap(), 100);
+  CHECK_EQ(jq.size(), 0);
+  CHECK_EQ(jq.cap(), 100);
 }
 
+TEST_CASE("TestJobQueueAdd") {
+  mt::job_queue<int> jq(100);
 
-TEST_CASE("TestJobQueueAdd"){
-    mt::job_queue<int> jq(100);
-
-    jq.add_job(5);
-    CHECK_EQ(jq.size(),1);
-    jq.add_job(10);
-    CHECK_EQ(jq.size(),2);
-
+  jq.add_job(5);
+  CHECK_EQ(jq.size(), 1);
+  jq.add_job(10);
+  CHECK_EQ(jq.size(), 2);
 }
-TEST_CASE("TestJobQueueAddAboveCap"){
-    mt::job_queue<int> jq(100);
-    for (int i =0;i<100;i++){
-        jq.add_job(1);
+TEST_CASE("TestJobQueueAddAboveCap") {
+  mt::job_queue<int> jq(100);
+  for (int i = 0; i < 100; i++) {
+    jq.add_job(1);
+  }
+  jq.add_job(2);
+  CHECK_EQ(jq.size(), 100);
+}
+
+TEST_CASE("TestJobQueue_I") {
+  mt::job_queue<int> jq(100);
+  int input_total = 0;
+  int output_total = 0;
+
+  auto producer = [&]() {
+    for (int i = 0; i < static_cast<int>(jq.cap()); ++i) {
+      jq.add_job(i);
+      input_total += i;
     }
-    jq.add_job(2);
-    CHECK_EQ(jq.size(),100);
-    
+  };
+
+  auto consumer = [&]() {
+    while (jq.size() > 0) {
+      output_total += jq.pop_job();
+    }
+  };
+
+  std::thread thr1(producer);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  std::thread thr2(consumer);
+
+  thr1.join();
+  thr2.join();
+
+  CHECK_EQ(input_total, output_total);
 }
 
-TEST_CASE("TestJobQueue_I")
-{
-    mt::job_queue<int> jq(100);
-    int input_total = 0;
-    int output_total = 0;
+TEST_CASE("TestJobQueuePop") {
+  mt::job_queue<int> jq(100);
 
-    auto producer = [&]() {
-        for (int i = 0; i < static_cast<int>(jq.cap()); ++i) {
-            jq.add_job(i);
-            input_total += i;
-        }
-    };
-
-    auto consumer = [&]() {
-        while (jq.size() > 0) {
-            output_total += jq.pop_job();
-        }
-    };
-
-    std::thread thr1(producer);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    std::thread thr2(consumer);
-
-    thr1.join();
-    thr2.join();
-    
-    CHECK_EQ(input_total, output_total);
+  jq.add_job(5);
+  jq.add_job(10);
+  int num = jq.pop_job();
+  CHECK_EQ(num, 5);
 }
+TEST_CASE("TestJobQueuePopEmptyQueue") {
+  mt::job_queue<int> jq(100);
 
+  jq.add_job(5);
+  int num = jq.pop_job();
+  CHECK_EQ(num, 5);
 
-
-TEST_CASE("TestJobQueuePop"){
-    mt::job_queue<int> jq(100);
-
-    jq.add_job(5);
-    jq.add_job(10);
-    int num = jq.pop_job();
-    CHECK_EQ(num,5);
-    
-}
-TEST_CASE("TestJobQueuePopEmptyQueue"){
-    mt::job_queue<int> jq(100);
-
-    jq.add_job(5);
-    int num = jq.pop_job();
-    CHECK_EQ(num,5);
-
-    num = jq.pop_job();
-    CHECK_EQ(num,0);
-    CHECK_EQ(jq.size(),0);
+  num = jq.pop_job();
+  CHECK_EQ(num, 0);
+  CHECK_EQ(jq.size(), 0);
 }
