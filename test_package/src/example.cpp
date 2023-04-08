f044d3afbfc881a924d6272c313ee3d489228b8d
Annie Song <afsong2018@gmail.com>
2023-04-08 07:51:34 -0400

    linter

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,7 +1,9 @@
 
 #include <job_queue.hpp>
 
-int main() {
-    mt::job_queue<int> jq(100);
+auto main() -> int {
+    constexpr size_t CAP = 100;
+
+    mt::job_queue<int> jq(CAP);
     return 0;
 }
