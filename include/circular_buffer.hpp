71ebd3df7af10ab23fe9828a1b0f400e3337d193
Annie Song <afsong2018@gmail.com>
2023-05-05 00:14:51 -0400

    add documentation for circular buffer

--- include/circular_buffer.hpp
+++ include/circular_buffer.hpp
@@ -7,16 +7,58 @@
 #include <queue>
 
 namespace mt {
+    /**
+     * @brief A generic circular buffer implementation. Similar to the job
+     * queue, it can be used to implement the consumer-producer pattern, but
+     * this implementation is cache-friendlier since we use a contiguous array
+     * as our data store.
+     *
+     * @tparam T, tye type of entry that is transferred between the consumers
+     * and producers.
+     */
     template <typename T> class circular_buffer {
       public:
+        /**
+         * @brief Construct a new circular buffer object
+         *
+         * @param cap capacity of the circular buffer
+         */
         circular_buffer(size_t cap);
 
+        /**
+         * @brief Add a new entry to the buffer
+         *
+         * @param entry new entry to be placed into the buffer
+         * @return true if entry was successfully added to the buffer
+         * @return false if entry failed to be added
+         */
         auto add_entry(const T& entry) -> bool;
+
+        /**
+         * @brief Get an entry from the buffer
+         *
+         * @return T is the entry returned. It's empty if failed to get entry
+         */
         auto get_entry() -> T;
 
+        /**
+         * @brief Mark the buffer as inactive and notify all waiters
+         *
+         */
         void stop_buffer();
 
+        /**
+         * @brief Return the number of entries in the buffer
+         *
+         * @return size_t, the size of the queue
+         */
         [[nodiscard]] auto size() const -> size_t;
+
+        /**
+         * @brief Return the capacity of the buffer
+         *
+         * @return size_t, the capacity of the buffer
+         */
         [[nodiscard]] auto cap() const -> size_t;
 
       private:
